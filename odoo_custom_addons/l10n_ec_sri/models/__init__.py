668f60805f3b430880ce077f6f5dcae7738154a8
somaplanet <cannachaincosta@gmail.com>
2026-01-24 16:04:05 -0500

    fix: Remove non-existent model imports from l10n_ec_sri

--- odoo_custom_addons/l10n_ec_sri/models/__init__.py
+++ odoo_custom_addons/l10n_ec_sri/models/__init__.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from . import res_company
-from . import l10n_ec_certificate
 from . import account_move
 from . import account_move_purchase
 from . import l10n_ec_sri_xml
-from . import sri_signer
-from . import sri_service
 from . import l10n_ec_retention
 from . import l10n_ec_sri_retention_xml
