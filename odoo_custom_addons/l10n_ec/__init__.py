609856e4396e7bc1dc024c94200c43651175d289
somaplanet <cannachaincosta@gmail.com>
2026-01-24 16:08:56 -0500

    fix: Add post_init_hook and uninstall_hook to l10n_ec

--- odoo_custom_addons/l10n_ec/__init__.py
+++ odoo_custom_addons/l10n_ec/__init__.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
 from . import wizard
+
+
+def post_init_hook(env):
+    """Post installation hook - set default config."""
+    env['ir.config_parameter'].sudo().set_param('l10n_ec.installed', 'True')
+
+
+def uninstall_hook(env):
+    """Cleanup on uninstall."""
+    pass
